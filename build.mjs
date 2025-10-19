01c1428c640a39bdaa39b70f99fce2a7eeece250
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2025-10-19 10:23:34 -0500

    spa test

--- build.mjs
+++ build.mjs
@@ -18,6 +18,7 @@ async function serve(ctx) {
 
     let { hosts, port } = await ctx.serve({
         servedir: "dist",
+        fallback: "dist/404.html",
         onRequest: r => console.log(`${r.method} ${r.path} ${r.status}`),
     });
 
@@ -26,9 +27,14 @@ async function serve(ctx) {
 
 const ctx = await build();
 
-if (process.argv[2] === "--serve") {
-    await serve(ctx);
-} else {
-    ctx.dispose();
+switch (process.argv[2]) {
+    case "--serve":
+        await serve(ctx);
+        break;
+    case "--watch":
+        await ctx.watch();
+        break;
+    default:
+        ctx.dispose();
+        break;
 }
-
--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,33 @@
 import { render } from "preact";
+import { LocationProvider, ErrorBoundary, Router, Route, useLocation } from "preact-iso";
 import "./app.css";
 
 import { Verify } from "./tools/verify.jsx";
 import { Menu } from "./menu.jsx";
 
+function Test() {
+    return <div>TEST</div>;
+}
+
+function NotFound() {
+    const location = useLocation();
+    console.log(location);
+    return <div>{location.path}</div>;
+}
+
 function App() {
-	return <main>
-        <Menu />
-        <Verify />
-    </main>;
+	return <LocationProvider>
+        <ErrorBoundary>
+            <main>
+                <Menu />
+                <Router>
+                    <Route path="verify" component={Verify} />
+                    <Route path="test" component={Test} />
+                    <Route default component={NotFound} />
+                </Router>
+            </main>
+        </ErrorBoundary>
+    </LocationProvider>;
 }
 
 render(<App />, document.body);
